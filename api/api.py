ac526e06513b5d3db1b5c6db575fe2e33819d17b
Aloba James <alobajames1@gmail.com>
2023-09-10 17:25:26 +0100

    new updates

--- api/api.py
+++ api/api.py
@@ -1,8 +1,29 @@
 import time
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/username', methods=['POST', 'GET', 'PUT'])
+def get_current_username():
+    if request.method == 'POST' or 'PUT':
+        try:
+            data = request.form.to_dict()
+            write_to_file(data)
+            return 'Done!'
+        except:
+            return 'Did not save to Database'
+    else:
+        return 'somethings wrong'
+
+def write_to_file(data):
+    with open('database.txt', 'a') as database:
+        username = data['email']
+        file = database.write(f'\n{username}')
+
+    # elif request.method == 'GET':
+    #     return {'username': username}
+
--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+
+const Dashboard = () => {
+  const [currentTime, setCurrentTime] = useState(0);
+  const [currentUserName, setCurrentUserName] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleChange = event => {
+    setMessage(event.target.value);
+    console.log('value is:', event.target.value);
+  };
+
+  const handleClick = event => {
+
+    setCurrentUserName(message);
+    console.log(
+      `{'message': ${message}}`
+    );
+    console.log('The link was clickedd.');
+  };
+
+  // const dataToUpdate = {
+  //   message: { message },
+  // };
+  // const jsonString = JSON.stringify(dataToUpdate);
+
+  // const url = 'username'; // Replace this with the real API endpoint
+
+  // const options = {
+  //   method: 'PUT',
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body: jsonString
+  // };
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+       fetch('/username', 'POST' ).then(res => res.json()).then(data => {
+      setCurrentUserName(data.username);
+    });
+
+
+    // fetch(url, options)
+    //   .then(response => {
+    //     if (!response.ok) {
+    //       throw new Error(`HTTP error ${response.status}`);
+    //     }
+    //     return response.json();
+    //   })
+    //   .then(updatedData => {
+    //     console.log('Data updated:', updatedData);
+    //   })
+    //   .catch(error => {
+    //     console.error('Error updating data:', error);
+    //   });
+  }, []);
+
+
+
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <p>The current time is {currentTime}.</p>
+          <p>The current name is {currentUserName}.</p>
+          <input
+            type="text"
+            id="message"
+            name="message"
+            onChange={handleChange}
+            value={message}
+          />
+
+          <div>
+            Edit user name
+            <button onClick={handleClick}>Done</button>
+          </div>
+        </header>
+      </div>
+    </>
+  )
+}
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route index
+          element={<Dashboard />}
+        />
+      </Routes>
+    </>
   );
 }
 
